63f4b757a0de45e9c8c32f312513658b26c78edf
Tang Ruiwen <wow.2007.t@163.com>
2020-06-02 00:11:35 +0800

    disable svga_init

--- kernel/pci/pci.c
+++ kernel/pci/pci.c
@@ -124,45 +124,46 @@ void pci_set_mem_enable(const pci_address_t* addr, int enable) {
     pci_config_write16(addr, offsetof(pci_config_space_t, command), command);
 }
 
-static void paint_screen(uint32_t color) {
-    uint32_t* fb = (uint32_t*)gSVGA.fb_mem;
-    int x, y;
-    static uint32_t fence = 0;
-    /*
-     * Flow control: Before we re-write the beginning of the
-     * framebuffer, make sure the host has at least finished drawing
-     * the beginning of the last frame.
-     *
-     * This isn't as strict as it should be: For proper 2D flow
-     * control, we really need to either do a full sync at the end of
-     * each frame, or double-buffer (alternating buffers on every other
-     * frame). This method can produce glitches, but it's still decent
-     * for benchmarking purposes.
-     */
+// static void paint_screen(uint32_t color) {
+//     uint32_t* fb = (uint32_t*)gSVGA.fb_mem;
+//     int x, y;
+//     static uint32_t fence = 0;
+//     /*
+//      * Flow control: Before we re-write the beginning of the
+//      * framebuffer, make sure the host has at least finished drawing
+//      * the beginning of the last frame.
+//      *
+//      * This isn't as strict as it should be: For proper 2D flow
+//      * control, we really need to either do a full sync at the end of
+//      * each frame, or double-buffer (alternating buffers on every other
+//      * frame). This method can produce glitches, but it's still decent
+//      * for benchmarking purposes.
+//      */
 
-    svga_sync_to_fence(fence);
-    fence = svga_insert_fence();
+//     svga_sync_to_fence(fence);
+//     fence = svga_insert_fence();
 
-    /*
-     * Update the screen, pixel by pixel.
-     */
+//     /*
+//      * Update the screen, pixel by pixel.
+//      */
 
-    for (y = 0; y < gSVGA.height; y++) {
-        uint32_t* row = fb;
-        fb = (uint32_t*)(gSVGA.pitch + (uint8_t*)fb);
+//     for (y = 0; y < gSVGA.height; y++) {
+//         uint32_t* row = fb;
+//         fb = (uint32_t*)(gSVGA.pitch + (uint8_t*)fb);
 
-        for (x = 0; x < gSVGA.width; x++) {
-            *(row++) = color;
-            svga_update(x, y, 1, 1);
-        }
-    }
-}
+//         for (x = 0; x < gSVGA.width; x++) {
+//             *(row++) = color;
+//             // svga_update(x, y, 1, 1);
+//         }
+//     }
+//     svga_update(0, 0, gSVGA.width, gSVGA.height);
+// }
 
 void pci_init(void) {
     kprintf("pci init ---------- \n");
-    svga_init();
-    svga_set_mode(1024, 768, 32);
-    while (1) {
-        paint_screen(0x747cba);
-    }
+    // svga_init();
+    // svga_set_mode(1024, 768, 32);
+    // while (1) {
+    //     paint_screen(0xffff0000);
+    // }
 }
